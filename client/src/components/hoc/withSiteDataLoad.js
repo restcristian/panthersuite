11f272878f71f9877baeba949ff8e13378a6cbc3
Cristian Restituyo <cristian.restituyo@denkwerk.com>
2020-05-27 22:42:17 +0200

    Optimized images and videos both on client and server

--- client/src/components/hoc/withSiteDataLoad.js
+++ client/src/components/hoc/withSiteDataLoad.js
@@ -1,51 +1,52 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { compose } from 'redux';
-import { allProgress } from '../../utils/helpers';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { compose } from "redux";
+import { allProgress } from "../../utils/helpers";
 
 import {
-    getEvents,
-    getConcertsEvents
-} from '../../store/actions/eventsActions';
-import { getSpotlight } from '../../store/actions/spotlightActions';
+  getEvents,
+  getConcertsEvents,
+} from "../../store/actions/eventsActions";
+import { getSpotlight } from "../../store/actions/spotlightActions";
 
-import LoadingScreen from '../LoadingScreen/LoadingScreen';
+import LoadingScreen from "../LoadingScreen/LoadingScreen";
 
 function withSiteDataLoad(WrappedComponent) {
-    return class extends Component {
-        state = {
-            dataProgress: 0
-        };
-        componentDidMount() {
-            allProgress(
-                [
-                    this.props.getEvents(),
-                    this.props.getSpotlight(),
-                    this.props.getConcertsEvents()
-                ],
-                progress => this.setState({ dataProgress: progress })
-            );
-        }
-        arePromisesDone = () => {
-            return this.state.dataProgress === 100;
-        }
-        render() {
-            return (
-                <React.Fragment>
-                    <LoadingScreen startLoading = {this.arePromisesDone()}/>
-                    <WrappedComponent dataProgress={this.state.dataProgress} />
-                </React.Fragment>
-            );
-        }
+  return class extends Component {
+    state = {
+      dataProgress: 0,
+    };
+    componentDidMount() {
+      allProgress(
+        [
+          this.props.getEvents(),
+          this.props.getSpotlight(),
+          this.props.getConcertsEvents(),
+        ],
+        (progress) => this.setState({ dataProgress: progress })
+      );
     }
+    arePromisesDone = () => {
+      return this.state.dataProgress === 100;
+    };
+    render() {
+      return (
+        <React.Fragment>
+          <LoadingScreen startLoading={this.arePromisesDone()} />
+          <WrappedComponent dataProgress={this.state.dataProgress} />
+        </React.Fragment>
+      );
+    }
+  };
 }
 
-const composedWithSiteLoad =
-    compose(connect(null,
-        {
-            getEvents,
-            getSpotlight,
-            getConcertsEvents
-        }), withSiteDataLoad);
+const composedWithSiteLoad = compose(
+  connect(null, {
+    getEvents,
+    getSpotlight,
+    getConcertsEvents,
+  }),
+  withSiteDataLoad
+);
 
 export default composedWithSiteLoad;
